81171d71e98d5925af4b2cc745c011192274ae92
wobin1 <nathanielmusa3@gmail.com>
2023-09-11 08:53:14 +0100

    minor edits

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutingService } from '../../services/routing-service/routing.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  constructor(private router: RoutingService){}
+
+
+  route(page:string){
+    console.log("route working")
+    this.router.route(page)
+  }
 
 }
